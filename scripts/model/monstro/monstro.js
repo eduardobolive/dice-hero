fbd7596bb54582dd93c792b9a6beb93566a2482b
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-05 00:30:45 -0300

    Inclusão de nome aleatório

--- scripts/model/monstro/monstro.js
+++ scripts/model/monstro/monstro.js
@@ -1,5 +1,6 @@
 export default class Monstro {
   constructor(nivel, classe) {
+    this.nome = getNomeAleatorio(classe);
     this.nivel = nivel;
     this.classe = classe;
     this.hp = +(50 * nivel * (1 + getRandomPercent(0, 20))).toFixed(2);
@@ -12,4 +13,34 @@ function getRandomPercent(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return (Math.floor(Math.random() * (max - min)) + min) / 100;
+}
+
+function getNomeAleatorio(classe) {
+  var listaNomes = [
+    "Jão",
+    "Tonho",
+    "Bicho",
+    "Corvo",
+    "Adapto",
+    "Guarda",
+    "Juninho",
+    "Cazé",
+    "Certezas",
+    "Ozob",
+    "Ruprest",
+    "Edsert",
+    "Cigado",
+    "Azaghal",
+    "Alan",
+  ];
+
+  var nome = listaNomes[Math.floor(Math.random() * listaNomes.length)];
+  var preposicao;
+  if (classe === "Fogo") {
+    preposicao = "do";
+  } else {
+    preposicao = "da";
+  }
+
+  return nome + " " + preposicao + " " + classe;
 }
