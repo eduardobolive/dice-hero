6f5b4b85ddd0d42ac4218f62add41562f6fb0648
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-06 15:57:26 -0300

    Adicionado elemento de tentativas

--- scripts/atualizaGame.js
+++ scripts/atualizaGame.js
@@ -1,5 +1,6 @@
 import Dinheiro from "./model/dinheiroModel.js";
 import LojaModel from "./model/lojaModel.js";
+import TentativaModel from "./model/tentativaModel.js";
 import PO from "./pageObjects.js";
 
 export default class AtualizaGame {
@@ -8,5 +9,6 @@ export default class AtualizaGame {
     PO.lojaAtaqueValor.innerHTML = LojaModel.valorUpgradeAtaque;
     PO.lojaUpgradeValor.innerHTML = LojaModel.valorUpgradeTentativa;
     PO.lojaCarteiraQtdMoedas.innerHTML = Dinheiro.moedas;
+    PO.campoTentativas.innerHTML = TentativaModel.tentativasRestantes;
   }
 }
