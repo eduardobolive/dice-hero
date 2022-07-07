d950c7755600272f881bb27cf59c0ef538417b48
Eduardo Oliveira <eduardobolive@gmail.com>
2022-07-07 15:55:18 -0300

    Ajuste ao pegar numero aleatório, não retornava o max

--- scripts/util.js
+++ scripts/util.js
@@ -1,5 +1,6 @@
 export default class Util {
   static getRandomInt(min, max) {
+    max++;
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
